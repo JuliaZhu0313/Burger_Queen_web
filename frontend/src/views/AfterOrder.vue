0a758202a208e71f873625b6e1cc230b1b76f613
ZHU XIAOXIAO <77180636+JuliaZhu0313@users.noreply.github.com>
2022-11-01 21:24:55 +0800

    Add files via upload
    New view page for rating

---
+++ frontend/src/views/AfterOrder.vue
@@ -0,0 +1,29 @@
+<template>
+    <img src="@/assets/logo.png" alt="Burger Queen" width="100" height="100" id="logo">
+    <NavBar />
+    <rating v-model="rating"/>
+  </template>
+  
+  <script>
+  import rating from '@/components/rating.vue'
+  import NavBar from '@/components/NavBar.vue'
+  
+  export default {
+    name: 'AfterOrder',
+    components: {
+      rating,
+      NavBar,
+    }
+  }
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
