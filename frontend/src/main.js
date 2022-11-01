16a553038a6df9f00aa3f0f246ab4b047f8dbbd4
ZHU XIAOXIAO <77180636+JuliaZhu0313@users.noreply.github.com>
2022-11-01 21:22:23 +0800

    Add files via upload
    New main.js with rating import

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
+import vue3starRatings from "vue3-star-ratings";
 
-createApp(App).use(router).mount('#app')
+createApp(App).component("vue3StarRatings", vue3starRatings).use(router).mount('#app')
