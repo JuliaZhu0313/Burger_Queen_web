9d980aadfd18f3c5320447976e41f7abaa66caf3
ZHU XIAOXIAO <77180636+JuliaZhu0313@users.noreply.github.com>
2022-11-01 21:25:52 +0800

    Add files via upload
    Rating page component

---
+++ frontend/src/components/rating.vue
@@ -0,0 +1,27 @@
+<template>
+    <h2>Please rate our service</h2>
+    <vue3starRatings v-model="rating" />
+    <button @click="Submit"> Submit </button>
+  </template>
+  
+  <script>
+  import { defineComponent } from "vue";
+  import vue3starRatings from "vue3-star-ratings";
+  
+  export default 
+    defineComponent({
+    components: {
+        vue3starRatings,
+    },
+    props: {
+      step: {
+      type: Number,
+      default: 0.5,
+    },},
+    methods: {
+    Submit() {
+        alert("Your rating has been submitted successfully! :) ")
+    }
+  }
+  });
+  </script>
