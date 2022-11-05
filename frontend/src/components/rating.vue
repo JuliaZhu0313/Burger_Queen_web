99461c6f794ca80c85df1ec9f13d103d6e4c2882
ZHU XIAOXIAO <77180636+JuliaZhu0313@users.noreply.github.com>
2022-11-05 20:18:28 +0800

    Add files via upload

--- frontend/src/components/rating.vue
+++ frontend/src/components/rating.vue
@@ -2,6 +2,10 @@
     <h2>Please rate our service</h2>
     <vue3starRatings v-model="rating" />
     <button @click="Submit"> Submit </button>
+    <button
+          @click="this.$router.push('/menu')" 
+          class="button"><h3>Go back to Menu</h3>
+    </button>
   </template>
   
   <script>
@@ -25,3 +29,18 @@
   }
   });
   </script>
+
+  <style scoped>
+  button {
+  background-color: #c77414;
+	color: white;
+  display:block;
+  border-radius: 12px;
+	font-weight: bold;
+  font-size: 15px;
+  padding: 15px;
+  width: 40%;
+  margin: auto;
+  margin-bottom: 5px;
+  }
+  </style>
