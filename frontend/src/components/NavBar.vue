63a3196b6c57e2c676dfded062a4c7b5743ae3b5
ZHU XIAOXIAO <77180636+JuliaZhu0313@users.noreply.github.com>
2022-11-05 20:20:00 +0800

    Update NavBar.vue

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -6,7 +6,6 @@
         <br>
         <button @click="personalCentre">Personal Centre</button>
         <br>
-        <router-link to="/afterorder"> Rating </router-link> | 
         <button @click="logOut">Log Out</button>
     </div>
   </div>
