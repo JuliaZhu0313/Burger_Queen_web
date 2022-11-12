a9f025508c22b17f16934a1be0730fb71a1c63c9
ZHU XIAOXIAO <77180636+JuliaZhu0313@users.noreply.github.com>
2022-11-12 22:01:08 +0800

    UI updated

--- frontend/src/components/Order.vue
+++ frontend/src/components/Order.vue
@@ -1,35 +1,37 @@
 <template v-if="user">
 <NavBar />
     <div class ="order">
+        <div class="div1">
           <h1 class="section-title">Current Ongoing orders</h1>
           <div class = "Time"> Food is on the way 
             <h3> Estimated Arrival Time is {{timestamp()}} to {{addMinutes()}} in 20 mins</h3>
             <!--<h3> Estimated Time taken is 20 mins </h3>-->
           </div>
-    <div class="Rec1">
-    <h1>Order Details</h1>
-    <table>
-        <div v-for = "index in len" :key="index" class="cart1">
-            <tr>
-                <div class="burger">
-                <td data-th="Product">
-                    <div class="row">
-                        <div>
-                            <h4>{{ value.name[index - 1] }}</h4>
-                        </div>
-                    </div>
-                </td>
-                <td data-th="Price">${{ value.price[index - 1] }}</td>
-                <td data-th="Quantity">Quantity: {{value.amount[index - 1]}}</td>
-                </div>
-            </tr>
         </div>
-    </table>
-    </div>
-    <br><br>
-    <div class="Rec2"><h1>Address details</h1>
-        <h4>{{profile.address_line_1}} , {{profile.address_line_2}}</h4>
-    </div>
+        <div class="Rec1">
+        <h1>Order Details</h1>
+        <table>
+            <div v-for = "index in len" :key="index" class="cart1">
+                <tr>
+                    <div class="burger">
+                    <td data-th="Product">
+                        <div class="row">
+                            <div>
+                                <h4>{{ value.name[index - 1] }}</h4>
+                            </div>
+                        </div>
+                    </td>
+                    <td data-th="Price">${{ value.price[index - 1] }}</td>
+                    <td data-th="Quantity">Quantity: {{value.amount[index - 1]}}</td>
+                    </div>
+                </tr>
+            </div>
+        </table>
+        </div>
+        <br><br>
+        <div class="Rec2"><h1>Address details</h1>
+            <h4>{{profile.address_line_1}} , {{profile.address_line_2}}</h4>
+        </div>
     </div>   
     
     </template>
@@ -116,26 +118,50 @@
     
     <style scoped>
     .order{
-     text-align: center;
-     
+    background-color:white;
+    /*display: flex;*/
+    align-items: center;
+    text-align: center;
+    min-height: 100vh;
+    border: 1px solid rgb(197, 197, 197);
+    box-shadow: -5px 12px 15px rgba(0, 0, 0, 0.2), 5px -5px 15px rgba(0, 0, 0, 0.19);
+    }
+    .div1{
+        width: 100%;
+        float: left;
+        height: 30%;
+        vertical-align: top;
     }
     .Rec1 {
-        position:absolute; 
+        /*position:absolute; 
         left:10px;
         width: 700px;
         background-color: bisque;
         border: 2px solid green ;
         font-size: 20px;
-        margin: auto;
+        margin: auto;*/
+        float: left;
+        width: 100%;
+        /*min-height: 48%;*/
+        background-color: bisque;
+        border: 2px solid green ;
+        font-size: 20px;
     }
     .Rec2 {
-        position:absolute; 
+        /*position:absolute; 
         right:10px;
         font-size: 18px;
         width: 500px;
         border: 1px solid green ;
         background-color: burlywood;
         border-radius: 12px;
-        margin: auto;
+        margin: auto;*/
+        float: left;
+        width: 100%;
+        /*min-height: 48%;*/
+        border: 1px solid green ;
+        background-color: burlywood;
+        border-radius: 12px;
+        font-size: 18px;
     }
     </style>
