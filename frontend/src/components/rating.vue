9141d8c3d83bfb58477ea0b2371424dbfc3292d7
ZHU XIAOXIAO <77180636+JuliaZhu0313@users.noreply.github.com>
2022-11-16 02:04:33 +0800

    new rating file

--- frontend/src/components/rating.vue
+++ frontend/src/components/rating.vue
@@ -1,46 +1,59 @@
 <template>
-    <h2>Please rate our service</h2>
-    <vue3starRatings v-model="rating" />
-    <button @click="Submit"> Submit </button>
+  <h3>Give us a rating of your ordering experience :) </h3>
+  <star-rating :increment="0.5" id="star"></star-rating>
+  <div class="btn1">
+  <button @click="Submit"> Submit </button></div>
+  <div class="btn2">
     <button
           @click="this.$router.push('/menu')" 
           class="button"><h3>Go back to Menu</h3>
     </button>
+  </div>
 </template>
-  
-  <script>
-  import { defineComponent } from "vue";
-  import vue3starRatings from "vue3-star-ratings";
-  
-  export default 
-    defineComponent({
-    components: {
-        vue3starRatings,
-    },
-    props: {
-      step: {
-      type: Number,
-      default: 0.5,
-    },},
-    methods: {
+
+<script>
+import StarRating from 'vue-star-rating'
+export default {
+  components: {
+    StarRating
+  },
+  methods: {
     Submit() {
         alert("Your rating has been submitted successfully! :) ")
     }
-  }
-  });
-  </script>
+  },
 
-  <style scoped>
-  button {
+}
+</script>
+
+<style>
+#star{
+  text-align: center;
+	align-items: center;
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+}
+.btn1 button {
   background-color: #c77414;
 	color: white;
-  display:block;
   border-radius: 12px;
 	font-weight: bold;
   font-size: 15px;
   padding: 15px;
-  width: 40%;
-  margin: auto;
+  width: 20%;
   margin-bottom: 5px;
+  margin: auto;
   }
-  </style>
+  .btn2 button {
+  background-color: #c77414;
+	color: white;
+  border-radius: 12px;
+	font-weight: bold;
+  font-size: 15px;
+  padding: 15px;
+  width: 20%;
+  margin-bottom: 5px;
+  margin: auto;
+  }
+</style>
